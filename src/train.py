c71ce197bd8ac4f01f08e8ad0f29e7c39b6a1db9
eugeniatate-duke <yt238@duke.edu>
2026-04-10 21:23:44 -0700

    replace LinReg with Ridge

--- src/train.py
+++ src/train.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 import numpy as np
 
@@ -29,18 +30,56 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# create model
-model = LinearRegression()
+# create RF model
+# model = RandomForestRegressor(
+#     n_estimators=50,  # small to keep fast
+#     max_depth=10,
+#     random_state=42
+# )
+
+# create LinReg model
+# model = LinearRegression()
+# model = Ridge(alpha=1.0)
+
+for alpha in [0.1, 1.0, 10.0]:
+    model = Ridge(alpha=alpha)
+    model.fit(X_train, y_train)
+    preds = model.predict(X_test)
+    mae = mean_absolute_error(y_test, preds)
+    rmse = np.sqrt(mean_squared_error(y_test, preds))
+    
+    print(f"alpha: {alpha}, rmse: {rmse}, mae: {mae}")
+    # show predictions vs actual values
+    results = pd.DataFrame({
+        "actual": y_test.values,
+        "predicted": preds
+    })
+
+    # show prediction error
+    results["error"] = results["actual"] - results["predicted"]
+
+    print(results.head(10))
 
 # train model
-model.fit(X_train, y_train)
+# model.fit(X_train, y_train)
 
 # get predictions
-preds = model.predict(X_test)
+# preds = model.predict(X_test)
 
 # evaluate MAE and RMSE 
-mae = mean_absolute_error(y_test, preds)
-rmse = np.sqrt(mean_squared_error(y_test, preds))
+# mae = mean_absolute_error(y_test, preds)
+# rmse = np.sqrt(mean_squared_error(y_test, preds))
 
-print("mae:", mae)
-print("rmse:", rmse)
+# print("mae:", mae)
+# print("rmse:", rmse)
+
+# show predictions vs actual values
+# results = pd.DataFrame({
+#     "actual": y_test.values,
+#     "predicted": preds
+# })
+
+# # show prediction error
+# results["error"] = results["actual"] - results["predicted"]
+
+# print(results.head(10))
